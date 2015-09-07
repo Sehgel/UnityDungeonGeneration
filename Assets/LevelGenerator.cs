1d7f730425e706dc1a85562e1ccbe1b988d4b919
Tremens8 <lucasemanuelgonzalez8@gmail.com>
2015-09-07 05:00:32 -0300

    Added trap and item generation

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -5,34 +5,33 @@ public class LevelGenerator : MonoBehaviour {
 
     public int mapSize = 100;
 
-    public Vector2[] map;
+    private Vector2[] map;
     public GameObject groundPrefab;
     public GameObject surroundingsPrefab;
+    public GameObject trapPrefab;
+    public GameObject itemPrefab;
+
+    public Vector2 spawnPosition = Vector2.zero;
+
+    [Range(0,100)]
+    public float trapsCoeficient = 60;
+    public float trapDistanceAwayFromSpawn = 5;
+
+    [Range(0, 100)]
+    public float itemsCoeficient = 60;
+    public float itemsDistanceAwayFromSpawn = 5;
 
 	void Start () {
 
         map = new Vector2[mapSize];
 
-        map[0] = new Vector2();
+        map[0] = spawnPosition;
         
         GameObject start =  ((GameObject)Instantiate(groundPrefab, Vector2.zero, Quaternion.identity));
         start.GetComponent<SpriteRenderer>().color = Color.green;
         StartCoroutine(run());
 	}
-    void GenerateSorroundings()
-    {
-        for (int i = 0; i < map.Length; i++)
-        {
-            if(!IsPositionTaken(map[i] + new Vector2(0,1)))
-                Instantiate(surroundingsPrefab, map[i] + new Vector2(0, 1), Quaternion.identity);
-            if (!IsPositionTaken(map[i] + new Vector2(0, -1)))
-                Instantiate(surroundingsPrefab, map[i] + new Vector2(0, -1), Quaternion.identity);
-            if (!IsPositionTaken(map[i] + new Vector2(1, 0)))
-                Instantiate(surroundingsPrefab, map[i] + new Vector2(1, 0), Quaternion.identity);
-            if (!IsPositionTaken(map[i] + new Vector2(-1, 0)))
-                Instantiate(surroundingsPrefab, map[i] + new Vector2(-1, 0), Quaternion.identity);
-        }
-    }
+
     IEnumerator run()
     {
         int counter = 1;
@@ -45,18 +44,89 @@ public class LevelGenerator : MonoBehaviour {
                 map[counter] = randomPos;
             }
             counter++;
-            yield return new WaitForSeconds(0.01f);
+            yield return null;
         }
         GameObject end = ((GameObject)Instantiate(groundPrefab, map[counter-1], Quaternion.identity));
         end.GetComponent<SpriteRenderer>().color = Color.red;
         GenerateSorroundings();
+        GenerateTraps();
+        GenerateItems();
         Debug.Log("Done!");
+    }
+
+    void GenerateSorroundings()
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (!IsPositionTaken(map[i] + new Vector2(0, 1)))
+                Instantiate(surroundingsPrefab, map[i] + new Vector2(0, 1), Quaternion.identity);
+            if (!IsPositionTaken(map[i] + new Vector2(0, -1)))
+                Instantiate(surroundingsPrefab, map[i] + new Vector2(0, -1), Quaternion.identity);
+            if (!IsPositionTaken(map[i] + new Vector2(1, 0)))
+                Instantiate(surroundingsPrefab, map[i] + new Vector2(1, 0), Quaternion.identity);
+            if (!IsPositionTaken(map[i] + new Vector2(-1, 0)))
+                Instantiate(surroundingsPrefab, map[i] + new Vector2(-1, 0), Quaternion.identity);
+        }
+    }
+
+    void GenerateTraps()
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (Random.Range(0, 100) < trapsCoeficient && GetDistanceToSpawn(map[i]) > trapDistanceAwayFromSpawn)
+            {
+                if (GetCrossNeighboursCount(map[i]) == 2 && GetXNeighboursCount(map[i]) == 0 )
+                {
+                    Instantiate(trapPrefab, map[i], Quaternion.identity);
+                }
+            }
+        }
+    }
+    private void GenerateItems()
+    {
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (Random.Range(0, 100) < itemsCoeficient && GetDistanceToSpawn(map[i]) > itemsDistanceAwayFromSpawn)
+            {
+                if (GetCrossNeighboursCount(map[i]) == 2 && GetXNeighboursCount(map[i]) == 1    )
+                {
+                    Instantiate(itemPrefab, map[i], Quaternion.identity);
+                }
+            }
+        }
+    }
+    private int GetCrossNeighboursCount(Vector2 _mapPosition)
+    {
+        int counter = 0;
+
+        if(IsPositionTaken(_mapPosition + new Vector2(0,1)))
+            counter++;
+        if (IsPositionTaken(_mapPosition + new Vector2(0, -1)))
+            counter++;
+        if (IsPositionTaken(_mapPosition + new Vector2(1, 0)))
+            counter++;
+        if (IsPositionTaken(_mapPosition + new Vector2(-1, 0)))
+            counter++;
+        return counter;
+    }
+    private int GetXNeighboursCount(Vector2 _mapPosition)
+    {
+        int counter = 0;
+        if (IsPositionTaken(_mapPosition + new Vector2(1, 1)))
+            counter++;
+        if (IsPositionTaken(_mapPosition + new Vector2(1, -1)))
+            counter++;
+        if (IsPositionTaken(_mapPosition + new Vector2(-1, 1)))
+            counter++;
+        if (IsPositionTaken(_mapPosition + new Vector2(-1, -1)))
+            counter++;
+        return counter;
     }
 
     private Vector2 GetRandomAdjacentTile(Vector2 _currentPosition)
     {
         int i = 0;
-        int multiplier = 2;
+        int multiplier = 1;
         Vector2 randomDirection = GetRandomDirection(_currentPosition);
         while (IsPositionTaken(randomDirection))
         {
@@ -88,14 +158,18 @@ public class LevelGenerator : MonoBehaviour {
         else
             return _currentPosition + new Vector2(0, i);
     }
+    private float GetDistanceToSpawn(Vector2 _position)
+    {
+        return (_position - spawnPosition).SqrMagnitude();
+    }
     private Vector2 GetRandomDirection(Vector2 _currentPosition)
     {
         Vector2 randomDirection = _currentPosition;
-        //Up?
+        //Vertical?
         if (Random.Range(0, 2) > 0)
-            randomDirection.y += (Random.Range(0, 2) > 0) ? 1 : -1 ;
-        else//Then Down
-            randomDirection.x += (Random.Range(0, 2) > 0) ? 1 : -1 ;
+            randomDirection.y += (Random.Range(0, 2) > 0) ? 1 : -1 ;//Positive? then negative
+        else//Then Horizontal
+            randomDirection.x += (Random.Range(0, 2) > 0) ? 1 : -1;//Positive? then negative
 
         return randomDirection ;
     }
